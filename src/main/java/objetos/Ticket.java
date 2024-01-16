01101b35d7f991e768c5f80ad87230d1e48b7101
yvalval1411 <yvalval1411@g.educaand.es>
2024-01-16 12:52:19 +0100

    Inicio de Proyecto

---
+++ src/main/java/objetos/Ticket.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package objetos;
+
+/**
+ *
+ * @author yumara
+ */
+public class Ticket {
+
+    
+}
