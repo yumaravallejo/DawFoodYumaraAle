d9ac40b6222afed888590abeaa29f261b79e0989
aluqmai2705 <aluqmai2705@g.educaand.es>
2024-01-23 12:46:14 +0100

    Solución de errores TarjetaCredito

--- src/main/java/principal/TarjetaCredito.java
+++ src/main/java/principal/TarjetaCredito.java
@@ -15,28 +15,35 @@ import java.util.Random;
  * @author yumara
  */
 public class TarjetaCredito {
-    
+
     private YearMonth fechaCaduc;
-    private int CVV;
+    private int[] CVV = new int[3];
     private double saldo;
-    private int[] digitos; //numero de la tarjeta
+    private int[] digitos = new int[16]; //numero de la tarjeta
     private final int TAM_TARJ = 16;
+    String saldoCompl;
+    
+    
 
     public TarjetaCredito() {
         Random rd = new Random();
-        int mes = rd.nextInt(1,13);
+        int mes = rd.nextInt(1, 13);
         int año = rd.nextInt(2020, 2035);
         this.fechaCaduc = YearMonth.of(año, mes);
         //Creamos el CVV
-        int numeroCVV1 = rd.nextInt(0,10);
-        int numeroCVV2 = rd.nextInt(0,10);
-        int numeroCVV3 = rd.nextInt(0,10);
-        this.CVV = numeroCVV1 + numeroCVV2 + numeroCVV3;
-        this.saldo = rd.nextDouble(0, 501);
-        this.digitos = new int[TAM_TARJ];
-        for (int i=0; i<TAM_TARJ; i++){
-            this.digitos[i] = rd.nextInt(0,10);
+
+        for (int i = 0; i < CVV.length; i++) {
+            CVV[i] = rd.nextInt(0, 10);
         }
+        for (int i = 0; i < digitos.length; i++) {
+            digitos[i] = rd.nextInt(0, 10);
+        }
+
+        this.CVV = CVV;
+        this.saldo = rd.nextDouble(0, 501);
+        this.digitos = digitos;
+        saldoCompl = """ 
+                     %.2f """.formatted(saldo);
     }
 
     public YearMonth getFechaCaduc() {
@@ -47,12 +54,10 @@ public class TarjetaCredito {
         this.fechaCaduc = fechaCaduc;
     }
 
-    public int getCVV() {
-        return CVV;
-    }
-
-    public void setCVV(int CVV) {
-        this.CVV = CVV;
+    public void getCVV() {
+        for (int i = 0; i < CVV.length; i++) {
+            System.out.println(CVV[i]);
+        }
     }
 
     public double getSaldo() {
@@ -66,40 +71,42 @@ public class TarjetaCredito {
     public int[] getDigitos() {
         return digitos;
     }
-    
+
     public int getDigito1() {
         return digitos[12];
     }
-    
+
     public int getDigito2() {
         return digitos[13];
     }
-    
+
     public int getDigito3() {
         return digitos[14];
     }
+
     public int getDigito4() {
         return digitos[15];
     }
-    
 
     public void setDigitos(int[] digitos) {
         this.digitos = digitos;
     }
-    
-    
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("TarjetaCredito{");
         sb.append("fechaCaduc=").append(fechaCaduc);
-        sb.append(", CVV=").append(CVV);
-        sb.append(", saldo=").append(saldo);
-        sb.append(", digitos=");
-        for (int i = 0; i < TAM_TARJ; i++) {
-            System.out.print(this.digitos[i]);
+        sb.append(", CVV=");
+        for (int i = 0; i < CVV.length; i++) {
+            sb.append(CVV[i]);
         }
+        sb.append(", saldo=").append(saldoCompl);
+        sb.append(" digitos=");
+        for (int i = 0; i < digitos.length; i++) {
+            sb.append(digitos[i]);
+        }
+
         sb.append('}');
         return sb.toString();
     }
@@ -130,14 +137,12 @@ public class TarjetaCredito {
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 59 * hash + Objects.hashCode(this.fechaCaduc);
-        hash = 59 * hash + this.CVV;
-        hash = 59 * hash + (int) (Double.doubleToLongBits(this.saldo) ^ (Double.doubleToLongBits(this.saldo) >>> 32));
-        hash = 59 * hash + Arrays.hashCode(this.digitos);
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.fechaCaduc);
+        hash = 41 * hash + Arrays.hashCode(this.CVV);
+        hash = 41 * hash + (int) (Double.doubleToLongBits(this.saldo) ^ (Double.doubleToLongBits(this.saldo) >>> 32));
+        hash = 41 * hash + Arrays.hashCode(this.digitos);
         return hash;
     }
-    
+
 }
-
-
