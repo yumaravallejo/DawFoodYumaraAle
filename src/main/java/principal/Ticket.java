93e93b7b9c9eb806b0a8ad80007734edc88c5a8d
aluqmai2705 <aluqmai2705@g.educaand.es>
2024-01-26 09:40:31 +0100

    Clase ticket creada

--- src/main/java/principal/Ticket.java
+++ src/main/java/principal/Ticket.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package principal;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  *
@@ -10,5 +13,59 @@ package principal;
  */
 public class Ticket {
 
-    
+    private UUID id;
+    private double Precio;
+    private LocalDateTime fechaOperacion;
+    private ArrayList<Productos> productosPedidos;
+
+    public Ticket(UUID id, double Precio, LocalDateTime fechaOperacion, ArrayList<Productos> productosPedidos) {
+        this.id = UUID.randomUUID();
+        this.Precio = Precio;
+        this.fechaOperacion = fechaOperacion;
+        this.productosPedidos = productosPedidos;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public LocalDateTime getFechaOperacion() {
+        return fechaOperacion;
+    }
+
+    public ArrayList<Productos> getProductosPedidos() {
+        return productosPedidos;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+
+    public void setFechaOperacion(LocalDateTime fechaOperacion) {
+        this.fechaOperacion = fechaOperacion;
+    }
+
+    public void setProductosPedidos(ArrayList<Productos> productosPedidos) {
+        this.productosPedidos = productosPedidos;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Ticket{");
+        sb.append("id=").append(id);
+        sb.append(", Precio=").append(Precio);
+        sb.append(", fechaOperacion=").append(fechaOperacion);
+        sb.append(", productosPedidos=").append(productosPedidos);
+        sb.append('}');
+        return sb.toString();
+    }
 }
